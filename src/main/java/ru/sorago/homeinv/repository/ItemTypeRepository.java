29a5af4dab4ef940414ace54aac93a7de15f513a
Sorago <linehalt10909@yandex.ru>
2021-03-18 23:53:43 +0300

    Typing testing

--- src/main/java/ru/sorago/homeinv/repository/ItemTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.sorago.homeinv.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import ru.sorago.homeinv.model.ItemType;
-
-@Repository
-public interface ItemTypeRepository extends CrudRepository<ItemType, Long> {
-}
